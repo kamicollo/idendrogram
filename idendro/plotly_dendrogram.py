a9523994b17cb39d3959e7e9ac489703cf547bf7
Aurimas Racas <mail@aurimas.eu>
2022-09-05 00:34:20 +0200

    Plotly conversion respects matplotlib colors

--- idendro/plotly_dendrogram.py
+++ idendro/plotly_dendrogram.py
@@ -6,6 +6,7 @@ from plotly import exceptions, optional_imports
 from plotly.graph_objs import graph_objs
 import numpy as np
 import scipy as scp
+from matplotlib.colors import to_hex
 
 
 def convert_scipy_to_plotly(scipy_dendrogram, width=np.inf, height=np.inf):
@@ -85,80 +86,6 @@ class _Dendrogram(object):
         self.layout = self.set_figure_layout(width, height)
         self.data = dd_traces
 
-    def get_color_dict(self, colorscale):
-        """
-        Returns colorscale used for dendrogram tree clusters.
-        :param (list) colorscale: Colors to use for the plot in rgb format.
-        :rtype (dict): A dict of default colors mapped to the user colorscale.
-        """
-
-        # These are the color codes returned for dendrograms
-        # We're replacing them with nicer colors
-        # This list is the colors that can be used by dendrogram, which were
-        # determined as the combination of the default above_threshold_color and
-        # the default color palette (see scipy/cluster/hierarchy.py)
-        d = {
-            "r": "red",
-            "g": "green",
-            "b": "blue",
-            "c": "cyan",
-            "m": "magenta",
-            "y": "yellow",
-            "k": "black",
-            # TODO: 'w' doesn't seem to be in the default color
-            # palette in scipy/cluster/hierarchy.py
-            "w": "white",
-        }
-        default_colors = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
-
-        if colorscale is None:
-            rgb_colorscale = [
-                "rgb(0,116,217)",  # blue
-                "rgb(35,205,205)",  # cyan
-                "rgb(61,153,112)",  # green
-                "rgb(140,35,35)",  # black
-                "rgb(133,20,75)",  # magenta
-                "rgb(255,65,54)",  # red
-                "rgb(255,255,255)",  # white
-                "rgb(255,220,0)",  # yellow
-            ]
-        else:
-            rgb_colorscale = colorscale
-
-        for i in range(len(default_colors.keys())):
-            k = list(default_colors.keys())[i]  # PY3 won't index keys
-            if i < len(rgb_colorscale):
-                default_colors[k] = rgb_colorscale[i]
-
-        # add support for cyclic format colors as introduced in scipy===1.5.0
-        # before this, the colors were named 'r', 'b', 'y' etc., now they are
-        # named 'C0', 'C1', etc. To keep the colors consistent regardless of the
-        # scipy version, we try as much as possible to map the new colors to the
-        # old colors
-        # this mapping was found by inpecting scipy/cluster/hierarchy.py (see
-        # comment above).
-        new_old_color_map = [
-            ("C0", "b"),
-            ("C1", "g"),
-            ("C2", "r"),
-            ("C3", "c"),
-            ("C4", "m"),
-            ("C5", "y"),
-            ("C6", "k"),
-            ("C7", "g"),
-            ("C8", "r"),
-            ("C9", "c"),
-        ]
-        for nc, oc in new_old_color_map:
-            try:
-                default_colors[nc] = default_colors[oc]
-            except KeyError:
-                # it could happen that the old color isn't found (if a custom
-                # colorscale was specified), in this case we set it to an
-                # arbitrary default.
-                default_colors[nc] = "rgb(0,116,217)"
-
-        return default_colors
 
     def set_axis_layout(self, axis_key):
         """
@@ -225,30 +152,26 @@ class _Dendrogram(object):
         icoord = scp.array(scipy_dendrogram["icoord"])
         dcoord = scp.array(scipy_dendrogram["dcoord"])
         ordered_labels = scp.array(scipy_dendrogram["ivl"])
-        color_list = scp.array(scipy_dendrogram["color_list"])
-        colors = self.get_color_dict(None)
+        link_colors = scipy_dendrogram["color_list"]
 
         trace_list = []
 
-        for i in range(len(icoord)):
+        for ic, dc, color in zip(icoord, dcoord, link_colors):
             # xs and ys are arrays of 4 points that make up the '∩' shapes
             # of the dendrogram tree
             if self.orientation in ["top", "bottom"]:
-                xs = icoord[i]
+                xs = ic
+                ys = dc
             else:
-                xs = dcoord[i]
+                xs = dc
+                ys = ic
 
-            if self.orientation in ["top", "bottom"]:
-                ys = dcoord[i]
-            else:
-                ys = icoord[i]
-            color_key = color_list[i]
             trace = dict(
                 type="scatter",
                 x=np.multiply(self.sign[self.xaxis], xs),
                 y=np.multiply(self.sign[self.yaxis], ys),
                 mode="lines",
-                marker=dict(color=colors[color_key]),
+                marker=dict(color=to_hex(color)),
                 text=None,
                 hoverinfo="text",
             )
