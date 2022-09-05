27b44d8d3d5d665fad127439ef0f4b0cc0c4bc0b
Aurimas Racas <mail@aurimas.eu>
2022-09-05 02:13:04 +0200

    working in all orientations (requires refactoring)

--- idendro/plotly_dendrogram.py
+++ idendro/plotly_dendrogram.py
@@ -9,12 +9,13 @@ import scipy as scp
 from matplotlib.colors import to_hex
 
 
-def convert_scipy_to_plotly(scipy_dendrogram, width=np.inf, height=np.inf):
+def convert_scipy_to_plotly(scipy_dendrogram, width=np.inf, height=np.inf, orientation='top'):
 
     dendrogram = _Dendrogram(
         scipy_dendrogram,
         height=height,
-        width=width
+        width=width,
+        orientation=orientation
     )
 
     return graph_objs.Figure(data=dendrogram.data, layout=dendrogram.layout)
@@ -27,103 +28,19 @@ class _Dendrogram(object):
     def __init__(
         self,
         scipy_dendrogram,
-        orientation="bottom",
-        labels=None,
-        width=np.inf,
-        height=np.inf,
-        xaxis="xaxis",
-        yaxis="yaxis",        
+        orientation,
+        width,
+        height,        
     ):
         self.orientation = orientation
-        self.labels = labels
-        self.xaxis = xaxis
-        self.yaxis = yaxis
-        self.data = []
-        self.leaves = []
-        self.sign = {self.xaxis: 1, self.yaxis: 1}
-        self.layout = {self.xaxis: {}, self.yaxis: {}}
-
-        if self.orientation in ["left", "bottom"]:
-            self.sign[self.xaxis] = 1
-        else:
-            self.sign[self.xaxis] = -1
-
-        if self.orientation in ["right", "bottom"]:
-            self.sign[self.yaxis] = 1
-        else:
-            self.sign[self.yaxis] = -1
+        self.layout = {'xaxis': {}, 'yaxis': {}}
         
+        dd_traces, ordered_leaf_positions = self.get_dendrogram_traces(scipy_dendrogram)
 
-        (dd_traces, xvals, yvals, ordered_labels, leaves) = self.get_dendrogram_traces(
-            scipy_dendrogram
-        )
-
-        self.labels = ordered_labels
-        self.leaves = leaves
-        yvals_flat = yvals.flatten()
-        xvals_flat = xvals.flatten()
-
-        self.zero_vals = []
-
-        for i in range(len(yvals_flat)):
-            if yvals_flat[i] == 0.0 and xvals_flat[i] not in self.zero_vals:
-                self.zero_vals.append(xvals_flat[i])
-
-        if len(self.zero_vals) > len(yvals) + 1:
-            # If the length of zero_vals is larger than the length of yvals,
-            # it means that there are wrong vals because of the identicial samples.
-            # Three and more identicial samples will make the yvals of spliting
-            # center into 0 and it will accidentally take it as leaves.
-            l_border = int(min(self.zero_vals))
-            r_border = int(max(self.zero_vals))
-            correct_leaves_pos = range(
-                l_border, r_border + 1, int((r_border - l_border) / len(yvals))
-            )
-            # Regenerating the leaves pos from the self.zero_vals with equally intervals.
-            self.zero_vals = [v for v in correct_leaves_pos]
-
-        self.zero_vals.sort()
-        self.layout = self.set_figure_layout(width, height)
+        ordered_leaf_labels = scp.array(scipy_dendrogram['ivl'])
         self.data = dd_traces
 
-
-    def set_axis_layout(self, axis_key):
-        """
-        Sets and returns default axis object for dendrogram figure.
-        :param (str) axis_key: E.g., 'xaxis', 'xaxis1', 'yaxis', yaxis1', etc.
-        :rtype (dict): An axis_key dictionary with set parameters.
-        """
-        axis_defaults = {
-            "type": "linear",
-            "ticks": "outside",
-            "mirror": "allticks",
-            "rangemode": "tozero",
-            "showticklabels": True,
-            "zeroline": False,
-            "showgrid": False,
-            "showline": True,
-        }
-
-        if len(self.labels) != 0:
-            axis_key_labels = self.xaxis
-            if self.orientation in ["left", "right"]:
-                axis_key_labels = self.yaxis
-            if axis_key_labels not in self.layout:
-                self.layout[axis_key_labels] = {}
-            self.layout[axis_key_labels]["tickvals"] = [
-                zv * self.sign[axis_key] for zv in self.zero_vals
-            ]
-            self.layout[axis_key_labels]["ticktext"] = self.labels
-            self.layout[axis_key_labels]["tickmode"] = "array"
-
-        self.layout[axis_key].update(axis_defaults)
-
-        return self.layout[axis_key]
-
-    def set_figure_layout(self, width, height):
-        """
-        Sets and returns default layout object for dendrogram figure.
-        """
+        #set general layout properties
         self.layout.update(
             {
                 "showlegend": False,
@@ -134,10 +51,40 @@ class _Dendrogram(object):
             }
         )
 
-        self.set_axis_layout(self.xaxis)
-        self.set_axis_layout(self.yaxis)
+        #set axis defaults
+        axis_defaults = {
+            "type": "linear",
+            "ticks": "outside",
+            "mirror": False,
+            "rangemode": "tozero",
+            "showticklabels": True,
+            "zeroline": False,
+            "showgrid": False,
+            "showline": True,
+            "tickangle": 0,
+        }
+        self.layout['yaxis'].update(axis_defaults)
+        self.layout['xaxis'].update(axis_defaults)
 
-        return self.layout
+        #deal with labels
+        label_axis = 'xaxis' if orientation in ['top', 'bottom'] else 'yaxis'
+        self.layout[label_axis].update({
+            "ticktext": ordered_leaf_labels,
+            "tickmode": "array",
+            "tickvals": ordered_leaf_positions,
+        })
+        if label_axis == 'xaxis':
+            self.layout['xaxis']['tickangle'] = 90
+
+        #deal with reversed axis
+        if self.orientation == 'bottom':
+            self.layout['yaxis']['autorange'] = 'reversed'
+            self.layout['xaxis']['side'] = 'top'
+
+        if self.orientation == 'left':
+            self.layout['xaxis']['autorange'] = 'reversed'
+            self.layout['yaxis']['side'] = 'right'
+
 
     def get_dendrogram_traces(
         self, scipy_dendrogram
@@ -146,19 +93,21 @@ class _Dendrogram(object):
         Calculates all the elements needed for plotting a dendrogram.
         :param (ndarray) X: Matrix of observations as array of arrays
         """
-        
-        P = scipy_dendrogram 
-                
+                        
         icoord = scp.array(scipy_dendrogram["icoord"])
         dcoord = scp.array(scipy_dendrogram["dcoord"])
-        ordered_labels = scp.array(scipy_dendrogram["ivl"])
         link_colors = scipy_dendrogram["color_list"]
 
         trace_list = []
+        leaf_positions = np.array([])
 
         for ic, dc, color in zip(icoord, dcoord, link_colors):
             # xs and ys are arrays of 4 points that make up the '∩' shapes
             # of the dendrogram tree
+
+            #if dc == 0, we're looking at a leaf position
+            leaf_positions = np.append(leaf_positions, ic[dc == 0])
+
             if self.orientation in ["top", "bottom"]:
                 xs = ic
                 ys = dc
@@ -168,27 +117,14 @@ class _Dendrogram(object):
 
             trace = dict(
                 type="scatter",
-                x=np.multiply(self.sign[self.xaxis], xs),
-                y=np.multiply(self.sign[self.yaxis], ys),
+                x=xs,
+                y=ys,
                 mode="lines",
                 marker=dict(color=to_hex(color)),
                 text=None,
                 hoverinfo="text",
             )
 
-            try:
-                x_index = int(self.xaxis[-1])
-            except ValueError:
-                x_index = ""
-
-            try:
-                y_index = int(self.yaxis[-1])
-            except ValueError:
-                y_index = ""
-
-            trace["xaxis"] = "x" + x_index
-            trace["yaxis"] = "y" + y_index
-
             trace_list.append(trace)
 
-        return trace_list, icoord, dcoord, ordered_labels, P["leaves"]
+        return trace_list, np.sort(leaf_positions)
