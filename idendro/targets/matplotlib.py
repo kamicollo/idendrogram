8eb07affd492df5b88722f1df97f798ef48fda47
Aurimas Racas <mail@aurimas.eu>
2022-10-22 17:01:32 -0400

    Typing fixes

--- idendro/targets/matplotlib.py
+++ idendro/targets/matplotlib.py
@@ -1,7 +1,7 @@
 import math
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 from ..containers import ClusterLink, ClusterNode, Dendrogram
-from matplotlib.axes import Axes
+from matplotlib.axes import Axes # type: ignore
 from typing import List
 
 class matplotlibConverter():
@@ -12,7 +12,7 @@ class matplotlibConverter():
         show_nodes: bool,
         width: float,
         height: float,
-    ) -> list:
+    ) -> Axes:
 
         ax = self.setup_layout(orientation=orientation, width=width, height=height, dendrogram=dendrogram)
         
@@ -28,7 +28,9 @@ class matplotlibConverter():
         if show_nodes:
             if not dendrogram.computed_nodes:
                 raise RuntimeError("Nodes were not computed in create_dendrogram() step, cannot show them")
-            self.plot_nodes(ax, x, y, dendrogram.nodes)                        
+            self.plot_nodes(ax, x, y, dendrogram.nodes)    
+
+        return ax                    
 
     def setup_layout(self, orientation: str, width: float, height: float, dendrogram: Dendrogram) -> Axes:
         
@@ -77,14 +79,14 @@ class matplotlibConverter():
 
         return ax
 
-    def plot_links(self, ax: Axes, x: str, y: str, links: List[ClusterLink]):        
+    def plot_links(self, ax: Axes, x: str, y: str, links: List[ClusterLink]) -> None:        
 
         for link in links: 
             if len(link.strokedash) > 1:
                 dash = (link.strokedash[0], link.strokedash[1:]) 
             else:
-                dash = link.strokedash          
-            plt.plot(
+                dash = link.strokedash #type: ignore   
+            ax.plot(
                 link.__getattribute__(x), 
                 link.__getattribute__(y), 
                 color=link.fillcolor,
@@ -94,22 +96,11 @@ class matplotlibConverter():
             )
 
 
-    def plot_nodes(self, ax: Axes, x: str, y: str, nodes: List[ClusterNode]):
+    def plot_nodes(self, ax: Axes, x: str, y: str, nodes: List[ClusterNode]) -> None:
         
         for node in nodes:
-            
-            # circle = Circle(
-            #     (node.__getattribute__(x), node.__getattribute__(y)),
-            #     facecolor = node.fillcolor,                
-            #     linewidth = 2,
-            #     alpha = node.opacity,
-            #     radius = node.radius,
-            #     edgecolor = node.edgecolor
-            # )
 
-            # ax.add_patch(circle)
-
-            plt.plot(
+            ax.plot(
                 node.__getattribute__(x), node.__getattribute__(y),
                 markerfacecolor = node.fillcolor,                
                 linewidth = 2,
@@ -119,7 +110,7 @@ class matplotlibConverter():
                 marker='o'
             )
                 
-            plt.text(
+            ax.text(
                 node.__getattribute__(x), 
                 node.__getattribute__(y), 
                 s = node.label, 
