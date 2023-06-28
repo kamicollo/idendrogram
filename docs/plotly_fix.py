16cc2a478ec8bf64222913c392a7e3d9ad5185f5
Aurimas Racas <mail@aurimas.eu>
2023-06-28 17:10:28 +0300

    fix regex

--- docs/plotly_fix.py
+++ docs/plotly_fix.py
@@ -3,6 +3,6 @@ import re
 def on_page_content(html: str, **kwargs) -> str:
     
     # Fix plots in jupyter notebook
-    html = re.sub('(?<=<script type="text\/javascript">)\s*?require\(\["plotly"\], function\(Plotly\) {\s*?(?=window\.PLOTLYENV)', "", html)
-    html = re.sub('\).then\(function\(\){.*?(?=<\/script>)', ')}', html, flags=re.S)    
+    html = re.sub(r'(?<=<script type="text\/javascript">)\s*?require\(\["plotly"\], function\(Plotly\) {\s*?(?=window\.PLOTLYENV)', "", html)
+    html = re.sub(r'\).then\(function\(\){.*?(?=<\/script>)', ')}', html, flags=re.S)    
     return html
